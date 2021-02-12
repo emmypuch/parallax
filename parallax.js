4dfe709d5758bf8bd4887126406425a703159eff
emmypuch <adesuyiomowunmi@gmail.com>
2021-02-12 22:49:06 +0100

    modified parallax

--- parallax.js
+++ parallax.js
@@ -2,3 +2,7 @@ $(".nav-burger-link").on("click", function () {
   $(".nav-burger-link").toggleClass("closed");
   $(".nav-menu").toggleClass("open");
 });
+
+window.addEventListener("scroll", function () {
+  const parallax = docuent.querySelector(".parallax");
+});
