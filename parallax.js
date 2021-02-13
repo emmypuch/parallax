ebeaadae59079296237e7b06aec2bea6f2aed74a
emmypuch <adesuyiomowunmi@gmail.com>
2021-02-13 17:53:42 +0100

    added portfolio section

--- parallax.js
+++ parallax.js
@@ -5,7 +5,30 @@ $(".nav-burger-link").on("click", function () {
 
 window.addEventListener("scroll", function () {
   const parallax = document.querySelector(".parallax");
-  let scrollPosition = window.pageYOffset;
+  let scrollPosition = window.pageYOffsetet;
 
   parallax.style.transform = "translateY(" + scrollPosition * 0.5 + "px)";
+});
+
+$(document).ready(function () {
+  $("input").click(function () {
+    document.body.classList.toggle("dark");
+  });
+  $(".list").click(function () {
+    const value = $(this).attr("data-filter");
+    if (value == "all") {
+      $(".card").fadeIn(400);
+      $("li.list.active").removeClass("active");
+      $(this).addClass("active");
+    } else {
+      $("li.list.active").removeClass("active");
+      $(this).addClass("active");
+      $(".card")
+        .not("." + value)
+        .fadeOut(400);
+      $(".card")
+        .filter("." + value)
+        .fadeIn(400);
+    }
+  });
 });
