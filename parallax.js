6b03bf2a6f49f10ceffcb04ef387e95d8f2692ac
emmypuch <adesuyiomowunmi@gmail.com>
2021-02-14 21:54:03 +0100

    added parallax images

--- parallax.js
+++ parallax.js
@@ -3,12 +3,6 @@ $(".nav-burger-link").on("click", function () {
   $(".nav-menu").toggleClass("open");
 });
 
-window.addEventListener("scroll", function () {
-  const parallax = document.querySelector(".parallax");
-  let scrollPosition = window.pageYOffset;
-  parallax.style.transform = "translateY(" + scrollPosition * 0.5 + "px)";
-});
-
 $(document).ready(function () {
   $("input").click(function () {
     document.body.classList.toggle("dark");
